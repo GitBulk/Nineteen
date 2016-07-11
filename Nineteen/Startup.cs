d3ecd466b45185a5e7633f9f9bf5d9e9d091377a
gitbulk <giacatiensinh@hotmail.com>
2016-07-11 12:20:24 +0700

    Add project files.

---
+++ Nineteen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nineteen.Startup))]
+namespace Nineteen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
