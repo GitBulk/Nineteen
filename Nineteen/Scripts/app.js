0a4686e3b3b17b3fb57517c5a166db0728997435
gitbulk <giacatiensinh@hotmail.com>
2016-07-27 13:13:30 +0700

    review 1

---
+++ Nineteen/Scripts/app.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="angular-route.min.js" />
+/// <reference path="angular-resource.min.js" />
+
+
+
+(function () {
+    angular.module("nineteenApp", ['ngResource', 'ngRoute'])
+    .config(config);
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider, $locationProvider) {
+
+        $locationProvider.html5Mode(true);
+    }
+})();
