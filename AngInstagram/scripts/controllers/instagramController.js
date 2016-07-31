5a82499d746b44fa5a568ec3ff4f47d983f336e7
gitbulk <giacatiensinh@hotmail.com>
2016-07-31 18:39:35 +0700

    remove Ang and Instagram

---
+++ AngInstagram/scripts/controllers/instagramController.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../angular.min.js" />
+(function () {
+
+    angular.module("instagramApp")
+    .controller("instagramController", instagramController);
+
+    instagramController.$inject = ["$scope", "instagramFactory"];
+
+    function instagramController($scope, instagramFactory) {
+        $scope.images = [];
+        instagramFactory.fetchData(function (data) {
+            console.log(data);
+            $scope.images = data;
+
+        });
+    }
+})();
---
+++ AngInstagram/scripts/services/instagramService.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../angular.min.js" />
+/// <reference path="../angular-resource.min.js" />
+
+
+
+(function () {
+    angular.module("instagramApp").factory("instagramFactory", instagramFactory);
+    instagramFactory.$inject = ["$http"];
+    function instagramFactory($http) {
+        var me = {};
+        me.fetchData = function (callback) {
+
+            var endPoint = "https://api.instagram.com/v1/media/popular?count=99&client_id=9a10fac5c3b44bb8a30130df42c410b5&callback=JSON_CALLBACK";
+
+            $http.jsonp(endPoint).success(function (response) {
+                callback(response.data);
+            }).error(function (err) {
+                console.log(err);
+            });
+        }
+
+        return me;
+    }
+})();
